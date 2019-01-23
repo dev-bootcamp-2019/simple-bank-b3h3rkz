ee8f4c5631cf776dab5190f89acc24a248bf295c
Bernard Parah <barjebernard@gmail.com>
2019-01-23 19:02:44 +0100

    bug fix

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -51,7 +51,7 @@ contract SimpleBank {
     /// @return The balance of the user
     // A SPECIAL KEYWORD prevents function from editing state variables;
     // allows function to run locally/off blockchain
-    function balance() view public returns (uint) {
+    function balance() public view returns (uint) {
         /* Get the balance of the sender of this transaction */
         return balances[msg.sender];
     }
